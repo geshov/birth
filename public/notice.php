9633da8f303fc7f1e298bcbf83567a3d68119b91
gesho <geshov@gmail.com>
2023-07-29 10:40:58 +0300

    fix array map

--- public/notice.php
+++ public/notice.php
@@ -1,15 +1,13 @@
 <?php
 
-$persons = [];
-
 $csv = file_get_contents("https://docs.google.com/spreadsheets/d/e/2PACX-1vT-5j3rZHVbVl3fdH6Up-V_eRkb35Qb6Hev1cY0FQgi6RKGrinIiJdDkBno-XxPHMpKO_3MK6Npwakb/pub?gid=0&single=true&output=csv");
 
 $lines = explode("\r\n", $csv);
 
-foreach ($lines as $line) {
+$persons = array_map(function($line) {
   $fields = explode(",", $line);
-  $persons[] = (object) ["name" => $fields[0], "birth" => $fields[1]];
-}
+  return (object) ["name" => $fields[0], "birth" => $fields[1]];
+}, $lines);
 
 // здесь нужно отфильтровать массив $persons по совпадению ДР с текущей датой
 // если сегодня никто не родился, то дальше ничего не далеть (exit)
