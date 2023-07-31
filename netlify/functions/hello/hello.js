fff3d6a40385123cd45c1de7e218b94b11da72bc
gesho <geshov@gmail.com>
2023-07-31 15:54:14 +0300

    fix

--- netlify/functions/hello/hello.js
+++
@@ -1,9 +0,0 @@
-const handler = async (event) => {
-  const subject = event.queryStringParameters.name || 'Voldemar'
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}`,
-  }
-}
-
-module.exports = { handler }
---
+++ netlify/functions/notice.js
@@ -0,0 +1,5 @@
+const handler = async (event) => {
+
+}
+
+module.exports = { handler }
