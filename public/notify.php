612686cae5832f28934f8d88a25f502ab64dfd82
gesho <geshov@gmail.com>
2023-08-17 12:45:03 +0300

    move addresses to google sheet

--- public/notify.php
+++ public/notify.php
@@ -13,19 +13,22 @@ require "credentials.php";
 $csv = file_get_contents("https://docs.google.com/spreadsheets/d/e/2PACX-1vT_Aywj-d6NZ0rp5LZS66WR6-ex_HH9Fkp9xx9nhPwI1LGA1OwR2Mmg90dUUttFByBl91NoVDcYghqh/pub?gid=0&single=true&output=csv");
 
 $rows = explode("\r\n", $csv);
-
 $persons = array_map(function($row) {
   $fields = explode(",", $row);
   if (!strtotime($fields[1])) return false;
   $name = $fields[0];
   $birth = new DateTime($fields[1]);
-  return (object) ["name" => $name, "birth" => $birth];
+  $addresses = array();
+  for ($i = 2; $i < count($fields); $i++) {
+    if (filter_var($fields[$i], FILTER_VALIDATE_EMAIL)) $addresses[] = $fields[$i];
+  }
+  return (object) ["name" => $name, "birth" => $birth, "addresses" => $addresses];
 }, $rows);
 
 $now = new DateTime();
 $borns = array_filter($persons, function($person) {
-  global $now;
   if (!$person) return false;
+  global $now;
   return $person->birth->format("n") == $now->format("n") && $person->birth->format("j") == $now->format("j");
 });
 if (empty($borns)) exit("Persons not found");
@@ -35,11 +38,14 @@ $idf = new IntlDateFormatter("ru_RU", IntlDateFormatter::LONG, IntlDateFormatter
 $subject = "Напоминание о ДР";
 $body = "<h2>Сегодня родились</h2>";
 
+$addresses = array();
 $body .= "<ul>";
 foreach ($borns as $born) {
   $body .= "<li>" . $born->name . " (" . $idf->format($born->birth) . ")</li>";
+  $addresses = array_merge($addresses, $born->addresses);
 }
 $body .= "</ul>";
+$addresses = array_unique($addresses);
 
 $body .= '<div><a href="https://birth.geshov.ru/">Посмотреть весь список</a></div>';
 
@@ -57,6 +63,7 @@ try {
   $mail->Encoding = "base64"; 
 
   $mail->setFrom($from);
+  $mail->addBCC($from);
   foreach($addresses as $address) {
     $mail->addAddress($address);
   }
--- src/pages/credentials.php.js
+++ src/pages/credentials.php.js
@@ -1,14 +1,7 @@
 const php = `<?php
 
-$password = "${import.meta.env.MAIL_PASSWORD}";
 $from = "${import.meta.env.MAIL_FROM}";
-
-$addresses = [
-  "${import.meta.env.MAIL_ADDRESS1}",
-  "${import.meta.env.MAIL_ADDRESS2}",
-  "${import.meta.env.MAIL_ADDRESS3}",
-  "${import.meta.env.MAIL_ADDRESS4}"
-];
+$password = "${import.meta.env.MAIL_PASSWORD}";
 `;
 
 export function get() {
