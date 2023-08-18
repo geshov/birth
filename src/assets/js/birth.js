ec46125f9e3acb43416ffa329288d7b3b30ac93a
gesho <geshov@gmail.com>
2023-08-18 17:29:39 +0300

    optimization

--- src/assets/js/birth.js
+++ src/assets/js/birth.js
@@ -4,9 +4,9 @@ class Birth {
 
   constructor(id, persons) {
     this.id = id;
-    this.valueNames = ["name", "birth", { name: "iso", attr: "data-iso" }];
+    this.valueNames = ["name", "rus", { name: "iso", attr: "data-iso" }];
     this.searchColumns = ["name"];
-    this.listItem = `<div class="flex justify-between py-4 border-b border-gray-600"><div class="name shrink"></div><div class="birth iso shrink-0"></div></div>`;
+    this.listItem = `<div class="flex justify-between py-4 border-b border-gray-600"><div class="name shrink"></div><div class="rus iso shrink-0"></div></div>`;
     this.paginationItem = `<li class="group"><a class="page w-10 h-10 bg-gray-700 text-gray-300 hover:bg-blue-600 hover:text-white group-[.active]:bg-blue-800 group-[.active]:text-gray-200 p-4 inline-flex items-center text-sm font-medium rounded-full transition-all" href="#"></a></li>`;
     this.paginationInner = 1;
     this.paginationOuter = 1;
@@ -49,14 +49,13 @@ class Birth {
 
   currentDay = (persons) => {
     const now = new Date();
-    let filtered = persons.filter(person =>
-      person.date.getDate() === now.getDate() && person.date.getMonth() === now.getMonth()
-    );
+    const birth = getDateFormat(now, "iso", "birth");
+    let filtered = persons.filter(person => person.birth === birth);
     if (filtered.length) {
       filtered = this.sortByName(filtered);
     } else {
-      const today = now.toLocaleDateString("ru-RU", { day: "numeric", month: "short" });
-      filtered = [{ name: this.emptyDay, birth: today }];
+      const rus = getDateFormat(now, "rus", "birth");
+      filtered = [{ name: this.emptyDay, rus: rus }];
     }
     return filtered;
   }
@@ -64,36 +63,29 @@ class Birth {
   nextWeek = (persons) => {
     const min = new Date();
     min.setDate(min.getDate() + 1);
-    let from = min.getMonth() * 100 + min.getDate();
+    const from = getDateFormat(min, "iso", "birth");
     const max = new Date();
     max.setDate(max.getDate() + 7);
-    let to = max.getMonth() * 100 + max.getDate();
-    let filtered = persons.filter(person => {
-      const birth = person.date.getMonth() * 100 + person.date.getDate();
-      return birth >= from && birth <= to;
-    });
+    const to = getDateFormat(max, "iso", "birth");
+    let filtered = persons.filter(person => person.birth >= from && person.birth <= to);
     if (filtered.length) {
-      filtered = this.sortByBirtn(filtered);
+      filtered = this.sortByBirth(filtered);
     } else {
-      from = min.toLocaleDateString("ru-RU", { day: "numeric", month: "short" });
-      to = max.toLocaleDateString("ru-RU", { day: "numeric", month: "short" });
-      const period = from + " - " + to;
-      filtered = [{ name: this.emptyWeek, birth: period }];
+      const rus = getDateFormat(min, "rus", "birth") + " - " + getDateFormat(max, "rus", "birth");
+      filtered = [{ name: this.emptyWeek, rus: rus }];
     }
     return filtered;
   }
 
   currentMonth = (persons) => {
     const now = new Date();
-    let filtered = persons.filter(person =>
-      person.date.getMonth() === now.getMonth()
-    );
+    const month = getDateFormat(now, "iso", "month");
+    let filtered = persons.filter(person => person.month === month);
     if (filtered.length) {
-      filtered = this.sortByBirtn(filtered);
+      filtered = this.sortByBirth(filtered);
     } else {
-      let month = now.toLocaleDateString("ru-RU", { month: "long" });
-      month = month[0].toUpperCase() + month.slice(1);
-      filtered = [{ name: this.emptyMonth, birth: month }];
+      const rus = getDateFormat(now, "rus", "month");
+      filtered = [{ name: this.emptyMonth, rus: rus }];
     }
     return filtered;
   }
@@ -106,12 +98,10 @@ class Birth {
     });
   }
 
-  sortByBirtn = (persons) => {
+  sortByBirth = (persons) => {
     return persons.sort((a, b) => {
-      const aa = a.date.getMonth() * 100 + a.date.getDate();
-      const bb = b.date.getMonth() * 100 + b.date.getDate();
-      if (aa > bb) return 1;
-      else if (aa < bb) return -1;
+      if (a.birth > b.birth) return 1;
+      else if (a.birth < b.birth) return -1;
       else return 0;
     });
   }
@@ -122,15 +112,50 @@ function getPersons(text) {
   const rows = text.split("\r\n");
   const persons = rows.map((row) => {
     const fields = row.split(",");
-    const name = fields[0];
     const dmy = fields[1].split(".");
     const date = new Date(Number(dmy[2]), Number(dmy[1]) - 1, Number(dmy[0]));
     if (isNaN(date.getTime())) return false;
-    const iso = date.toISOString();
-    const birth = date.toLocaleDateString("ru-RU", { day: "numeric", month: "short", year: "numeric" });
-    return { name: name, birth: birth, date: date, iso: iso };
+    const name = fields[0];
+    const rus = getDateFormat(date, "rus");
+    const iso = getDateFormat(date, "iso");
+    const birth = getDateFormat(date, "iso", "birth");
+    const month = getDateFormat(date, "iso", "month");
+    return { name: name, rus: rus, iso: iso, birth: birth, month: month };
   });
   return persons.filter(person => person);
+}
+
+function getDateFormat(date, format = "rus", part = "full") {
+  if (format == "iso") {
+    let parts;
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    switch (part) {
+      case "birth":
+        parts = [month, day];
+        break;
+      case "month":
+        parts = [month];
+        break;
+      default:
+        parts = [year, month, day];
+    }
+    return parts.join("-");
+  } else {
+    let options;
+    switch (part) {
+      case "birth":
+        options = { day: "numeric", month: "short" };
+        break;
+      case "month":
+        options = { month: "long" };
+        break;
+      default:
+        options = { day: "numeric", month: "short", year: "numeric" };
+    }
+    return date.toLocaleDateString("ru-RU", options);
+  }
 }
 
 fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vT_Aywj-d6NZ0rp5LZS66WR6-ex_HH9Fkp9xx9nhPwI1LGA1OwR2Mmg90dUUttFByBl91NoVDcYghqh/pub?gid=0&single=true&output=csv")
