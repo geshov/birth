2d19f3959b18f78d41c165258a61c151359f72c9
gesho <geshov@gmail.com>
2023-09-13 17:34:23 +0300

    upgrade to Astro 3

--- src/pages/credentials.php.js
+++ src/pages/credentials.php.js
@@ -4,8 +4,6 @@ $from = "${import.meta.env.MAIL_FROM}";
 $password = "${import.meta.env.MAIL_PASSWORD}";
 `;
 
-export function get() {
-  return {
-    body: php
-  };
+export function GET() {
+  return new Response(php);
 }
