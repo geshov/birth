e8d285bf99b007d39febf17668c7f407f1ecbe7c
gesho <geshov@gmail.com>
2023-09-18 10:32:49 +0300

    otimize metrika

--- src/assets/js/metrika.js
+++ src/assets/js/metrika.js
@@ -1,13 +1,21 @@
-(function (m, e, t, r, i, k, a) {
-  m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-  m[i].l = 1 * new Date();
-  for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-  k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-})
-  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+(() => {
+  if (
+    navigator.platform === "Linux x86_64" &&
+    navigator.language === "en-US" &&
+    history.length === 2
+  ) return;
 
-ym(90307050, "init", {
-  clickmap: true,
-  trackLinks: true,
-  accurateTrackBounce: true
-});
+  (function (m, e, t, r, i, k, a) {
+    m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+    m[i].l = 1 * new Date();
+    for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+  })
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+  ym(90307050, "init", {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true
+  });
+})();
